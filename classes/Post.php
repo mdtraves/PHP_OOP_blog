d5a921e39d9f1b98506859d6a04edc0595a66f27
MikeTraves <mdtraves@hotmail.com>
2021-04-05 23:22:31 +0100

    css and routing alterations

--- classes/Post.php
+++ classes/Post.php
@@ -13,19 +13,21 @@ class Post {
     <div class='col'>
         <div class='card my-2'>
             <div class='card-body'>
-                <h2 class='card-title'>" . $row["post_title"]. "</h2>
+                <a href='#'><h4 class='card-title mb-3'>" . $row["post_title"]. "</h4></a>
                 <p class='card-text'> " . $row["post_body"]. "</p>
-                <form action='deletePost.php' method='post'>
-                <input type='hidden' name='id' value='" . $row["id"] ."'>
-                <input type='submit' class='btn-danger' value='delete'>
-                </form>
+                <div class='btn-holder'>
                 <form action='edit.php?id=" .  $row["id"] . "' method='post'>
                 <input type='hidden' name='id' value='" . $row["id"] ."'>
                 <input type='hidden' name='title' value='" . $row["post_title"] ."'>
                 <input type='hidden' name='body' value='" . $row["post_body"] ."'>
-                <input type='submit' class='btn-info' value='edit'>
+                <button type='submit' ><img class='post-icon' src='/assets/images/edit.svg' />
                 </form>
-                
+                <form action='deletePost.php' method='post'>
+                <input type='hidden' name='id' value='" . $row["id"] ."'>
+                <button type='submit'><img class='post-icon' src='/assets/images/delete.svg' />
+                </button
+                </form>
+                </div>
             </div>
         </div>
     </div>
--- index.php
+++ index.php
@@ -9,31 +9,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/jpg" href="assets/images/cms.png"/>
-    <link rel="stylesheet" href="assets/css/styles.css"/>
+    <link rel="shortcut icon" type="image/jpg" href="/assets/images/logo.svg"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/styles.css"/>
+
     <title>CMS</title>
 </head>
-<body>
+<body class="bg-dark">
   
 <?php include('nav.php'); ?>
 
 <section>
   <div class="container">
-    <h1>PHP OOP CRUD</h1>
+    <div class="card p-4">
+    <h1 class="mb-4">PHP OOP CRUD</h1>
     <form method="post" action="addPost.php">
       <div class="mb-3">
         <label for="title" class="form-label">Title</label>
-        <input type="text" class="form-control" id="title" aria-describedby="title" name="title">
-        <div id="titlelHelp" class="form-text">Use a descriptive title</div>
+        <input type="text" class="form-control mb-4" id="title" aria-describedby="title" name="title">
       </div>
       <div class="mb-3">
         <label for="post" class="form-label">Post</label>
-        <textarea class="form-control" id="post" name="post" rows="4" cols="50"></textarea>
-        <div id="postHelp" class="form-text">Don't waffle on...</div>
+        <textarea class="form-control mb-4" id="post" name="post" rows="4" cols="50"></textarea>
  
       </div>
-      <button type="submit" class="btn btn-primary mb-3">Submit</button>
+      <button type="submit" class="btn btn-primary">Add post</button>
       <br>
       <?php 
       if(isset($_GET['create_success'])){
@@ -56,13 +56,13 @@
   </div>
   </form>
   </div>
+  </div>
 </section>
-<section>
 
+
+<section class="pt-0">
 <div class="container">
 <div class='row row-cols-1 row-cols-md-2 g-4'>
-  
-
 <?php 
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
